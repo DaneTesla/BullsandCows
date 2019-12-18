654a090a1c109341609da9a488a4ed57aab80251
DaneTesla <56448721+DaneTesla@users.noreply.github.com>
2019-12-18 01:55:05 +0100

    FINAL COMMIT
    final game

--- Section_02/BullsAndCowsGame/main.cpp
+++ Section_02/BullsAndCowsGame/main.cpp
@@ -3,19 +3,24 @@ This acts as the view in a MVC pattern, and is responsible for all
 user interaction. For game logic see the FBullCowGame class.
 */
 
+#pragma once
+
 #include <iostream>
 #include <string>
 #include "FBullCowGame.h"
 
+// to make syntax Unreal friendly
 using FText = std::string;
 using int32 = int;
 
+// function prototypes as outside class
 void PrintIntro();
 void PlayGame();
-FText GetGuess();
+FText GetValidGuess();
 bool AskToPlayAgain();
+void PrintGameSummary();
 
-FBullCowGame BCGame;//instatiate a new game
+FBullCowGame BCGame;//instatiate a new game, which I re-use across plays
 
 // the entry point for our app
 int main()
@@ -30,55 +35,92 @@ int main()
 	return 0; //exit app
 }
 
-
-
-// game intro
 void PrintIntro() 
 {
-	constexpr int32 WORD_LENGTH = 9;
-	std::cout << "Welcome to Bulls and Cows, traveller. It's a fun word game.\n";
-	std::cout << "Can you guess the " << WORD_LENGTH;
+	
+	std::cout << "Welcome to Bools and Couts (aka Bulls and Cows), a fun word game.\n";
+	std::cout << std::endl;
+	std::cout << "          }   {         ___ " << std::endl;
+	std::cout << "          (o o)        (o o) " << std::endl;
+	std::cout << "   /-------\\ /          \\ /-------\\ " << std::endl;
+	std::cout << "  / | BOOL |O            O| COUT  | \\ " << std::endl;
+	std::cout << " *  |-,--- |              |------|  * " << std::endl;
+	std::cout << "    ^      ^              ^      ^ " << std::endl;
+	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength();
 	std::cout << " letter isogram I'm thinking of?\n";
 	std::cout << std::endl;
 	return;
 }
 
+//plays a single game to completion
 void PlayGame()
 {
 	BCGame.Reset();
 	int32 MaxTries = BCGame.GetMaxTries();
 
 	// loop for the number of turns asking for guess
-	//TODO change from FOR to WHILE loop once we are validating tries
-	for (int32 count = 1; count <= MaxTries; count++) {
-		FText Guess = GetGuess(); // TODO make loop checking valid
+	while (!BCGame.IsGameWon() && BCGame.GetCurrentTry() <= MaxTries) {
+		FText Guess = GetValidGuess();
 
-		//submit valid guess to the game and receive count
-		FBullCowCount BullCowCount = BCGame.SubmitGuess(Guess);
-		//print number of cows and bulls
+		// submit valid guess to the game, and receive counts
+		FBullCowCount BullCowCount = BCGame.SubmitValidGuess(Guess);
+
 		std::cout << "Bulls = " << BullCowCount.Bulls;
-		std::cout << ". Cows = " << BullCowCount.Cows << std::endl;
-		std::cout << std::endl;
+		std::cout << ". Cows = " << BullCowCount.Cows << "\n\n";
 	}
+	PrintGameSummary();
+	return;
 }
 
-//TODO summarize game
 
-// player guess
-FText GetGuess() 
+
+//loop continually untill user gives a valid guess
+FText GetValidGuess()
 {
-	int32 CurrentTry = BCGame.GetCurrentTry();
-
-	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
 	FText Guess = "";
+EGuessStatus Status = EGuessStatus::Invalid_Status;
+do {
+	// get a guess from the player
+	int32 CurrentTry = BCGame.GetCurrentTry();
+	std::cout << "Try " << CurrentTry << " of " << BCGame.GetMaxTries();
+	std::cout << ". Enter your guess: ";
 	std::getline(std::cin, Guess);
-	return Guess;
+
+	// check status and give feedback
+	Status = BCGame.CheckGuessValidity(Guess);
+	switch (Status) {
+	case EGuessStatus::Wrong_Length:
+		std::cout << "Please enter a " << BCGame.GetHiddenWordLength() << " letter word.\n\n";
+		break;
+	case EGuessStatus::Not_Isogram:
+		std::cout << "Please enter a word witout repeating letters.\n\n";
+		break;
+	case EGuessStatus::Not_Lowercase:
+		std::cout << "Please enter all lowercase letters.\n\n";
+		break;
+	default:
+		// assume the guess is valid
+		break;
+	}
+} while (Status != EGuessStatus::OK); // keep looping until we get no errors
+return Guess;
 }
 
 bool AskToPlayAgain()
 {
-	std::cout << "Do you want to play again? y/n ";
+	std::cout << "Do you want to play again with the same hidden word (y/n)? ";
 	FText Response = "";
 	std::getline(std::cin, Response);
 	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
+
+void PrintGameSummary()
+{
+	if (BCGame.IsGameWon())
+	{
+		std::cout << "WELL DONE - YOU WIN!\n";
+	}
+	else {
+		std::cout << "YOU LOSE! Better luck next time.\n";
+	}
 }
